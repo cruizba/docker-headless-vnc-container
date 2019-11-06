8db0c40512c7f2a5b0f115f80f01837b89910371
cruizba <charlesrb94@outlook.com>
2019-11-06 21:59:12 +0100

    Adapted

---
+++ travis/push-images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
+DIST=$1
+DESKTOP=$2
+BASE_NAME="cruizba/ptaki-$DIST-$DESKTOP-vnc:${TRAVIS_TAG:-$TRAVIS_BRANCH}"
+
+if [[ -z "$DIST" ]] || [[ -z $DESKTOP ]]; then
+    echo "You must specify a distribution and a desktop environment" >&2
+    exit 1
+fi
+
+echo "Pushing in docker hub $BASE_NAME"
+docker push $BASE_NAME
