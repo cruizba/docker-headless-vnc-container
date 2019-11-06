e8850e2ee8e5dd2097ea5f63de679e6b6c854405
cruizba <charlesrb94@outlook.com>
2019-11-06 23:25:02 +0100

    Installing numpy for vnc centos 8

--- src/centos/install/tools.sh
+++ src/centos/install/tools.sh
@@ -3,8 +3,11 @@
 set -e
 
 echo "Install some common tools for further installation"
-yum -y install epel-release 
+yum -y install epel-release
 yum -y update
 yum -y install vim sudo wget which net-tools bzip2 \
-    numpy #used for websockify/novnc
+    pip3 #used for websockify/novnc
+
+pip3 install numpy #used for websockify/novnc
+
 yum clean all
